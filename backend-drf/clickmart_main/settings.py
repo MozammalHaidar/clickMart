d7ea7788d855777314b8b61efb736f4a2d429e08
MozammalHaidar <156530339+MozammalHaidar@users.noreply.github.com>
2026-01-13 23:40:45 +0600

    reset docker

--- backend-drf/clickmart_main/settings.py
+++ backend-drf/clickmart_main/settings.py
@@ -137,11 +137,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR/'static'
-STATICFILES_DIRS = [
-    'clickmart_main/static'
-]
+# STATICFILES_DIRS = [
+#     'clickmart_main/static'
+# ]
 
 AUTH_USER_MODEL = "users.User"
 
